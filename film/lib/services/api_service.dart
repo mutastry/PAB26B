bd6de58ebc7e6d87e6d88e0f89b696d185f35d21
Art <mutiaraastri2707@gmail.com>
2025-02-28 10:59:41 +0700

    film

---
+++ film/lib/services/api_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = '3f532695bc2452d24f8e1f6950944b9d';
+
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response = await
+    http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  Future<List<Map<String, dynamic>>> getTrendingMovies() async {
+    final response = await
+    http.get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  Future<List<Map<String, dynamic>>> getPopularMovies() async {
+    final response = await
+    http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+}
